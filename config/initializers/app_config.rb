5b95225c70d0c06f607003d348dae02e2a4b5590
joaopfsilva <j0a0.silva.kle@gmail.com>
2017-12-05 15:38:24 +0100

    comment app_config initi

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,5 +1,5 @@
-require 'yaml'
+# require 'yaml'
 
 # yaml_data = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'application.yml'))).result)
 # APP_CONFIG = HashWithIndifferentAccess.new(yaml_data)
-CREDENTIALS = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env].to_hash
+# CREDENTIALS = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env].to_hash
